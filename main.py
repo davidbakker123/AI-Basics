5b2bf555684c7a0caf62956cf63770077675f511
davidbakker123 <44167113+davidbakker123@users.noreply.github.com>
2021-07-26 23:57:25 +0200

    Can make cool pictures now, sometimes...

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 input_size = 2
 output_size = 1
-layers = 20
-nodes_per_layer = 10
+layers = 50
+nodes_per_layer = 50
 batch_size = 1000
 
-f = lambda x: np.where(x < 0, 0, x) 
+f = lambda x: np.where(x < 0, 0, x)  #1 / (1 + np.exp(-x)) # 
 
 grid = np.zeros(shape = [batch_size, batch_size, 2])
 for j in range(batch_size):
     for i in range(batch_size):
-        grid[j, i, 0] = j
-        grid[j, i, 1] = i
+        grid[j, i, 0], grid[j, i, 1] = j, i
 
+grid /= batch_size
 ### Initialize weights and biases ###
 w = []
 b = []
 
-w.append(np.random.normal(0, 1, size = [input_size, nodes_per_layer]))
-b.append(np.random.normal(0, 1, size = [batch_size, nodes_per_layer]))
+min = -1
+max = 1
+uni = lambda x: np.random.uniform(min, max, size = x)
+
+w.append(uni([input_size, nodes_per_layer]))
+b.append(uni([1, nodes_per_layer]))
 for i in range(layers - 2):
-    w.append(np.random.normal(0, 1, size = [nodes_per_layer, nodes_per_layer]))
-    b.append(np.random.normal(0, 1, size = [batch_size, nodes_per_layer]))
-w.append(np.random.normal(0, 1, size = [nodes_per_layer, output_size]))
-b.append(np.random.normal(0, 1, size = [batch_size, output_size]))
+    w.append(uni([nodes_per_layer, nodes_per_layer]))
+    b.append(uni([1, nodes_per_layer]))
+w.append(uni([nodes_per_layer, output_size]))
+b.append(uni([1, output_size]))
 
 ### Go through network ###
 z = np.zeros(shape = [batch_size, batch_size])
 
 for j in range(batch_size):
     input_vec = grid[j, :]
-    for i in range(layers - 1):
-        out = np.matmul(input_vec, w[i]) + b[i]
-        out = f(out)
+    for i in range(layers):
+        out = f(np.matmul(input_vec, w[i]) + b[i])
         input_vec = out
-    out = np.matmul(input_vec, w[-1]) + b[-1]
     z[:, j] = np.transpose(out)
 
 plt.imshow(z)
